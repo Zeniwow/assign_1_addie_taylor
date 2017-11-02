f08558edbc138a08f5705ce780c26db1eafc6561
SC\taylor.jurgensen <zeniwow@gmail.com>
2017-11-02 08:35:13 -0500

    final fixes

--- fuccios/resources/navigation.php
+++ fuccios/resources/navigation.php
@@ -1,6 +1,6 @@
 <ul class="nav navbar-nav">        
-             <li><a href='login.html'>Login</a></li>
-             <li><a href='sign_up.html'>Sign Up Here</a></li>
+             <li><a href='login.php'>Login</a></li>
+             <li><a href='sign_up.php'>Sign Up Here</a></li>
              <li><a href='menu.php'>Menu</a></li>
              <li><a href='about.php'>About/Contact Us</a></li>
              <li><a href='events.php'>Events</a></li>
